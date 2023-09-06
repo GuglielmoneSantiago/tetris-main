17cd3d5cb199c6ac3d695dec2a97e91a805dbd85
GuglielmoneSantiago <guglielmonesantiago69@gmail.com>
2023-09-06 12:20:05 -0300

    ver2

---
+++ Test/Piezas/PieceDogTest.java
@@ -0,0 +1,19 @@
+package Test.Piezas;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+
+import piezas.*;
+import Tetris;
+
+public class PieceDogTest {
+    @Test
+    public void Crear_PiezaDog(){
+       PieceDog p=new PieceDog();
+       assertEquals( 1, p.getValor(1,0));
+       assertEquals( 1, p.getValor(1,1));
+       assertEquals( 1, p.getValor(2,1));
+       assertEquals( 1, p.getValor(2,2)); 
+    }
+}
